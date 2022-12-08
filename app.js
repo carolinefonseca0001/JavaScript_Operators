39d69fe726f6812f220e4f3e5d593c1c57c3ca30
Caroline <caroline.fonseca0001@gmail.com>
2022-12-07 19:33:28 -0500

    completed exercises

--- app.js
+++ app.js
@@ -5,15 +5,81 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a = 20
+let b = 4
+let add = (a + b)
+let minus = (a - b)
+let multiply = (a*b)
+let dividing = (a/b)
 
-// YOUR CODE HERE
+console.log(`The value of a is ${a}`)
+console.log(`The value of b is ${b}`)
+console.log(`a + b = ${add} (20 + 4 = 24)`)
+console.log(`a - b = ${minus} (20 - 4 = 16)`)
+console.log(`a * b = ${multiply} (20 * 4 = 80)`)
+console.log(`a / b = ${dividing} (20 / 4 = 5)`)
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// YOUR CODE HERE
+console.log(`What is the value of: num + str?`);
+console.log(num+str)
+
+console.log('What is the value of: num + str2?');
+console.log(num+str2);
+
+console.log('What is the value of: num + isPresent?')
+console.log(num + isPresent);
+
+console.log('What is the value of: firstName + num?')
+console.log(firstName + num);
+
+console.log('What is the value of: isPresent + str?')
+console.log(isPresent + str);
+
+console.log('What is the value of: firstName + lastName?')
+console.log(firstName +  lastName);
+
+
+
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+console.log("EXERCISE 3:\n==========\n")
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
 
-// YOUR CODE HERE
+
+console.log('What is the value of: val == str3?')
+console.log(val == str3)
+
+console.log('What is the value of: val === str3?')
+console.log(val === str3);
+
+console.log(`What is the value of: !isAwake?`)
+console.log(!isAwake)
+
+console.log(`What is the value of: ("eleven" == str4 && val >= str3)?`)
+console.log('eleven' == str4 && val >= str3)
+
+console.log(`What is the value of: (!isAwake || isAwake)?`)
+console.log(!isAwake || isAwake)
+
+console.log(`What is the value of: 0 == false?`)
+console.log(0 == false)
+
+console.log('What is the value of: 0 === false?')
+console.log(0 === false)
+
+console.log('What is the value of: 0 != false?')
+console.log(0 != false)
+
+console.log('What is the value of 0 !== false?')
+console.log(0 !== false)
